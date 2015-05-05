c73a95fb1edfbc19a4807d07779ab02d0816fa8b
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2015-05-06 00:41:18 +0200

    Add ghetto synchronous mail notifications
    Should use queues for this tomorrow.

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -6,6 +6,9 @@ class ParticipationsController < ApplicationController
     participation = @challenge.participations.build(user: current_user)
     if participation.save
       flash[:success] = "You are now attending this challenge."
+      @challenge.users.each do |user|
+        NotificationMailer.participant_list_email(user, @challenge).deliver_now
+      end
     else
       flash[:error] = "Failed to attend challenge. "
       flash[:error] << participation.errors.full_messages.join(" ")
@@ -17,6 +20,10 @@ class ParticipationsController < ApplicationController
   def destroy
     @challenge = Challenge.find(params[:challenge_id])
     current_user.challenges.destroy(@challenge)
+
+    @challenge.users.each do |user|
+      NotificationMailer.participant_list_email(user, @challenge).deliver_now
+    end
 
     flash[:success] = "You are no longer attending this challenge."
     redirect_to @challenge
